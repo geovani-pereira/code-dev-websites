d89ac44fe96794e1b0261ff807fbad7a186f4f56
alex <alex@alexmpprog@gmail.com>
2017-07-19 10:36:55 -0300

    Adicionando o arquivo js apenas no mobile

---
+++ addArchiveInMobile.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  (function() {
+    var $width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    var $script = document.createElement("script");
+    $script.setAttribute("src", $width < 640 ? "http://confiancemoveis.com.br/js/jsMobileButton.js" : "");
+    document.body.appendChild($script);
+  })();
+</script>
